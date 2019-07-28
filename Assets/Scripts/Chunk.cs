bbf7b22e8a0d16a2876acb06aa27b14fb73161a6
Nicolas Perrier <nicolas.perrier2@numericable.fr>
2019-07-28 22:31:13 +0200

    Switch to HDRP

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,66 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Chunk : MonoBehaviour {
+public class Chunk : MonoBehaviour
+{
     public Vector3Int coord;
 
     [HideInInspector]
     public Mesh mesh;
 
-    MeshFilter meshFilter;
-    MeshRenderer meshRenderer;
-    MeshCollider meshCollider;
-    bool generateCollider;
+    private MeshFilter _meshFilter;
+    private MeshRenderer _meshRenderer;
+    private MeshCollider _meshCollider;
+    private bool _generateCollider;
 
-    public void DestroyOrDisable () {
-        if (Application.isPlaying) {
-            mesh.Clear ();
-            gameObject.SetActive (false);
-        } else {
-            DestroyImmediate (gameObject, false);
+    public void DestroyOrDisable()
+    {
+        if (Application.isPlaying)
+        {
+            mesh.Clear();
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            DestroyImmediate(gameObject, false);
         }
     }
 
     // Add components/get references in case lost (references can be lost when working in the editor)
-    public void SetUp (Material mat, bool generateCollider) {
-        this.generateCollider = generateCollider;
+    public void SetUp(Material mat, bool generateCollider)
+    {
+        _generateCollider = generateCollider;
 
-        meshFilter = GetComponent<MeshFilter> ();
-        meshRenderer = GetComponent<MeshRenderer> ();
-        meshCollider = GetComponent<MeshCollider> ();
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshCollider = GetComponent<MeshCollider>();
 
-        if (meshFilter == null) {
-            meshFilter = gameObject.AddComponent<MeshFilter> ();
+        if (_meshFilter == null)
+        {
+            _meshFilter = gameObject.AddComponent<MeshFilter>();
         }
 
-        if (meshRenderer == null) {
-            meshRenderer = gameObject.AddComponent<MeshRenderer> ();
+        if (_meshRenderer == null)
+        {
+            _meshRenderer = gameObject.AddComponent<MeshRenderer>();
         }
 
-        if (meshCollider == null && generateCollider) {
-            meshCollider = gameObject.AddComponent<MeshCollider> ();
+        if (_meshCollider == null && generateCollider)
+        {
+            _meshCollider = gameObject.AddComponent<MeshCollider>();
         }
-        if (meshCollider != null && !generateCollider) {
-            DestroyImmediate (meshCollider);
+        if (_meshCollider != null && !generateCollider)
+        {
+            DestroyImmediate(_meshCollider);
         }
 
-        mesh = meshFilter.sharedMesh;
-        if (mesh == null) {
-            mesh = new Mesh ();
+        mesh = _meshFilter.sharedMesh;
+        if (mesh == null)
+        {
+            mesh = new Mesh();
             mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
-            meshFilter.sharedMesh = mesh;
+            _meshFilter.sharedMesh = mesh;
         }
 
-        if (generateCollider) {
-            if (meshCollider.sharedMesh == null) {
-                meshCollider.sharedMesh = mesh;
+        if (generateCollider)
+        {
+            if (_meshCollider.sharedMesh == null)
+            {
+                _meshCollider.sharedMesh = mesh;
             }
             // force update
-            meshCollider.enabled = false;
-            meshCollider.enabled = true;
+            _meshCollider.enabled = false;
+            _meshCollider.enabled = true;
         }
 
-        meshRenderer.material = mat;
+        _meshRenderer.material = mat;
     }
 }
--- Assets/Scripts/ColourGenerator.cs
+++ Assets/Scripts/ColourGenerator.cs
@@ -1,44 +1,51 @@
 ﻿using UnityEngine;
 
 [ExecuteInEditMode]
-public class ColourGenerator : MonoBehaviour {
+public class ColourGenerator : MonoBehaviour
+{
     public Material mat;
     public Gradient gradient;
     public float normalOffsetWeight;
 
-    Texture2D texture;
-    const int textureResolution = 50;
+    private Texture2D _texture;
+    private const int TextureResolution = 50;
 
-    void Init () {
-        if (texture == null || texture.width != textureResolution) {
-            texture = new Texture2D (textureResolution, 1, TextureFormat.RGBA32, false);
+    private void Init()
+    {
+        if (_texture == null || _texture.width != TextureResolution)
+        {
+            _texture = new Texture2D(TextureResolution, 1, TextureFormat.RGBA32, false);
         }
     }
 
-    void Update () {
-        Init ();
-        UpdateTexture ();
+    private void Update()
+    {
+        Init();
+        UpdateTexture();
 
-        MeshGenerator m = FindObjectOfType<MeshGenerator> ();
+        MeshGenerator m = FindObjectOfType<MeshGenerator>();
 
         float boundsY = m.BoundsSize * m.NumChunks.y;
 
-        mat.SetFloat ("boundsY", boundsY);
-        mat.SetFloat ("normalOffsetWeight", normalOffsetWeight);
+        mat.SetFloat("_boundsY", boundsY);
+        mat.SetFloat("_normalOffsetWeight", normalOffsetWeight);
 
-        mat.SetTexture ("ramp", texture);
+        mat.SetTexture("_ramp", _texture);
     }
 
-    void UpdateTexture () {
-        if (gradient != null) {
-            Color[] colours = new Color[texture.width];
-            for (int i = 0; i < textureResolution; i++) {
-                Color gradientCol = gradient.Evaluate (i / (textureResolution - 1f));
+    private void UpdateTexture()
+    {
+        if (gradient != null)
+        {
+            Color[] colours = new Color[_texture.width];
+            for (int i = 0; i < TextureResolution; i++)
+            {
+                Color gradientCol = gradient.Evaluate(i / (TextureResolution - 1f));
                 colours[i] = gradientCol;
             }
 
-            texture.SetPixels (colours);
-            texture.Apply ();
+            _texture.SetPixels(colours);
+            _texture.Apply();
         }
     }
 }
